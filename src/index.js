026350699b8df5cfd73974eca0b63bbd6927cc73
SooHyun-Kang <k.josephine19@gmail.com>
2023-05-22 22:41:41 -0700

    AppForm: form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMentorsImmer from './AppMentorsImmer';
+import AppForm from './AppForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsImmer />
+    <AppForm />
   </React.StrictMode>
 );
 
