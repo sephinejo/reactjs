fdae640e6148deef46e6256d86b470646fae803c
SooHyun-Kang <k.josephine19@gmail.com>
2023-05-24 08:39:53 -0700

    css parts practice

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className='text-8xl'>Hi!</h1>
+      <button className='bg-blue-300 rounded-lg px-2'>Button</button>
+    </div>
+  );
+}
